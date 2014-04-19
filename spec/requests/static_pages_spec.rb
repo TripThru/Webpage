38c391f99a37ba1313828397c82c68fc68693e36
Daniel Montes de Oca <danielmontesdeoca@Daniels-MacBook-Pro.local>
2014-04-19 01:15:54 -0700

    improved test and fixed title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static pages" do
 
     it "should have the content 'TripThru'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title('TripThru | Home')
     end
   end
 end
