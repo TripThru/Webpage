24edb406eef6bdd8db60fadfb80d030cc0d4c12b
Daniel Montes de Oca <danielmontesdeoca@Daniels-MacBook-Pro.local>
2014-05-02 12:53:10 -0700

    Added setup to deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,48 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
+
+desc "Upload database.yml file."
+task :upload_yml do
+on roles(:app) do
+  execute "mkdir -p #{shared_path}/config"
+  upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
+end
+end
+
+desc "Upload nginx.conf file."
+task :upload_nginx do
+on roles(:app) do
+  execute "mkdir -p #{shared_path}/config"
+  upload! StringIO.new(File.read("config/nginx.conf")), "#{shared_path}/config/nginx.conf"
+end
+end
+
+desc "Upload unicorn.rb file."
+task :upload_unicorn do
+on roles(:app) do
+  execute "mkdir -p #{shared_path}/config"
+  upload! StringIO.new(File.read("config/unicorn.rb")), "#{shared_path}/config/unicorn.rb"
+end
+end
+
+desc "Upload unicorn_init file."
+task :upload_unicorn_init do
+on roles(:app) do
+  execute "mkdir -p #{shared_path}/config"
+  upload! StringIO.new(File.read("config/unicorn_init.sh")), "#{shared_path}/config/unicorn_init.sh"
+end
+end
+
+desc "Symlinks config files for Nginx and Unicorn."
+task :symlink_config do
+on roles(:app) do
+  execute "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
+  execute "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-available/#{fetch(:application)}"
+  execute "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{fetch(:application)}"
+end
+end
+
 set :application, "tripthru"
 set :deploy_to, "/var/www/#{fetch(:application)}"
 set :deploy_via, :remote_cache
